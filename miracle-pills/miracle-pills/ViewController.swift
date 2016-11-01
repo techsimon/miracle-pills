c8c6c704e3ec6aca76e3aef6cb258879f9aa02e0
techsimon <simon@techsimon.com>
2016-11-01 19:44:51 +0000

    created project

---
+++ miracle-pills/miracle-pills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miracle-pills
+//
+//  Created by Simon Smith on 01/11/2016.
+//  Copyright © 2016 tech simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
